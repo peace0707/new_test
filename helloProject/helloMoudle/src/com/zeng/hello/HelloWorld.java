d3ca9f6c27793ab7eb1ad8c7ffb97a4a26884269
zeal_peace <luming12138404@gmail.com>
2021-11-30 22:38:27 +0800

    第一次提交

---
+++ helloProject/helloMoudle/src/com/zeng/hello/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.zeng.hello;
+
+/**
+ * @description:
+ * @author: Administrator
+ * @date: 2021/11/25 22:21
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("这个世界会好的！！！");
+        System.out.println("hello world！！！");
+        System.out.println("hhhhhh");
+        System.out.println("我是老二");
+        System.out.println("我是老大");
+        System.out.println("哈哈哈哈哈哈");
+        System.out.println("哼哼哈嘿");
+        System.out.println("嚯嚯嚯");
+        System.out.println("我是猪八戒");
+        System.out.println("我是大师兄");
+        System.out.println("师父师父");
+        System.out.println("我要回高老庄");
+        System.out.println("妖怪哪里跑");
+        System.out.println("呆子哪里跑");
+		
+		
+    }
+}
